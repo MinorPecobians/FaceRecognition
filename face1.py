b816db572e9eea03ac2fd1647580337526435a07
Taran Mittal <taranmittal.1717@gmail.com>
2022-11-04 00:39:31 +0530

    name changes

--- face1.py
+++ face1.py
@@ -11,17 +11,17 @@ imgvirat = cv2.cvtColor(imgvirat_bgr , cv2.COLOR_BGR2RGB)
 # # cv2.waitKey(0)
 
 
-face = face_recognition.face_locations(imgvirat)[0]
-copy = imgvirat.copy()
+# face = face_recognition.face_locations(imgvirat)[0]
+# copy = imgvirat.copy()
 
-cv2.rectangle(copy, (face[3], face[0]), (face[1], face[2]),(255,0,255) ,2)
+# cv2.rectangle(copy, (face[3], face[0]), (face[1], face[2]),(255,0,255) ,2)
 # cv2.imshow('copy' , copy)
 # cv2.imshow('virat' , imgvirat)
 # cv2.waitKey(0)
 
 
 train_encode = face_recognition.face_encodings(imgvirat)[0]
-
+# compare image of virat with lebron
 test = face_recognition.load_image_file('img/unknown/basketball.jpg')
 test = cv2.cvtColor(test , cv2.COLOR_BGR2RGB)
 test_encode = face_recognition.face_encodings(test)[0]

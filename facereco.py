247f21e34edd86901a26d71a55281635281f8c2f
Taran Mittal <taranmittal.1717@gmail.com>
2022-11-11 01:11:48 +0530

    changes

--- facereco.py
+++ facereco.py
@@ -29,23 +29,16 @@ for cl in mylist:
 encoded_face_train = findEncodings(images)
 
 
-def markAttendance(name):
-    with open('Attendance.csv','r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-            now = datetime.now()
-            time = now.strftime('%I:%M:%S:%p')
-            date = now.strftime('%d-%B-%Y')
-            f.writelines(f'n{name}, {time}, {date}')
 
 
 cap  = cv2.VideoCapture(0)
+
+# infinite loop to keep capturing the frames 
 while True:
+
     success, img = cap.read()
+
+
     imgS = cv2.resize(img, (0,0), None, 0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
     faces_in_frame = face_recognition.face_locations(imgS)
@@ -54,8 +47,8 @@ while True:
         matches = face_recognition.compare_faces(encoded_face_train, encode_face)
         faceDist = face_recognition.face_distance(encoded_face_train, encode_face)
         matchIndex = np.argmin(faceDist)
-        print(matchIndex)
-        if matches[matchIndex]:
+        # print(matchIndex)
+        if (matches[matchIndex]) and (faceDist[matchIndex] < 0.4):
             name = classNames[matchIndex].upper().lower()
             y1,x2,y2,x1 = faceloc
             # since we scaled down by 4 times
@@ -63,7 +56,10 @@ while True:
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img, (x1,y2-35),(x2,y2), (0,255,0), cv2.FILLED)
             cv2.putText(img,name, (x1+6,y2-5), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
-            markAttendance(name)
+        # else:
+        #     print("unknown person")
+        #     break
+            # markAttendance(name)
     cv2.imshow('webcam', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
